5ce94cb653f984eba8863b7df2661e26a068ba3e
taurusboong2 <taurusboong2@gmail.com>
2022-01-28 21:16:10 +0900

    output detail page success

--- src/ArticleDetail.js
+++ src/ArticleDetail.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const ArticleDetail = ({ match }) => {
-  console.log(match.params);
+  const [type, setType] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [des, setDes] = useState(null);
+  const [create, setCreate] = useState(null);
+  const [update, setUpdate] = useState(null);
+
+  const putData = async () => {
+    const id = match.params.id;
+    try {
+      const response = await axios.get(`http://localhost:1337/api/articles/${id}`);
+      return response.data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  putData().then(res => {
+    const data = res.data.attributes;
+    setType(data.type);
+    setTitle(data.title);
+    setAuthor(data.author);
+    setDes(data.description);
+    setCreate(data.createdAt);
+    setUpdate(data.updatedAt);
+  });
+
   return (
     <div>
       <header id="detail_header">
@@ -9,25 +36,23 @@ const ArticleDetail = ({ match }) => {
       </header>
       <main>
         <div id="detail_wrap">
-          <p id="detail_type" />
-          <h2 id="detail_title" />
-          <h3 id="detail_author" />
-          <p id="detail_des" />
+          <p id="detail_type">{type}</p>
+          <h2 id="detail_title">{title}</h2>
+          <h3 id="detail_author">{author}</h3>
+          <p id="detail_des">{des}</p>
           <div id="time_wrap">
             <p id="detail_create">
-              생성시간:
+              생성시간:{create}
               <b id="create_data" />
             </p>
             <p id="detail_update">
-              수정시간:
+              수정시간:{update}
               <b id="update_data" />
             </p>
           </div>
         </div>
         <div id="btn_wrap">
-          <div id="delete_btn" onClick="deleteData()">
-            삭제
-          </div>
+          <div id="delete_btn">삭제</div>
           <a id="update_btn" href="">
             수정
           </a>
